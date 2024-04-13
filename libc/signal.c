3e24b4ac5a57cf5436399d77eea5adb0e46e6378
Peter Ross <pross@xvid.org>
2024-04-13 10:04:33 +1000

    libc: drop signal FIXME

--- libc/signal.c
+++ libc/signal.c
@@ -2,19 +2,12 @@
 
 int sigaddset(sigset_t *set, int signo)
 {
-#if 0
-    if (signo <= 0 || signo >= 64) {
-        errno = EINVAL;
-        return -1;
-    }
-#endif
     *set |= 1 << signo;
     return 0;
 }
 
 int sigdelset(sigset_t * set, int signo)
 {
-    //FIXME: errno
     *set &= ~(1 << signo);
     return 0;
 }
@@ -33,6 +26,5 @@ int sigfillset(sigset_t * set)
 
 int sigismember(const sigset_t *set, int signo)
 {
-    //FIXME: errnp
     return !!(*set & (1 << signo));
 }
