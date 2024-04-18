1e2e2f7727eb83a2fc2fc3aa851b1b97b2f80297
Peter Ross <pross@xvid.org>
2024-04-18 20:52:52 +1000

    programs: vi

--- programs/box_vi.c
+++ programs/box_vi.c
@@ -1,7 +1,9 @@
 #include <bsd/string.h>
+#include <ctype.h>
 #include <fcntl.h>
 #include <limits.h>
 #include <signal.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -27,14 +29,28 @@ typedef struct {
     int cursor;
     char message[64];
     char prompt[64];
+    unsigned int count;
+
+    int redraw;
+    int last_origin;
+    int last_endp;
 } State;
 
 /* return < 0 if cursor on first line, > 0 if cursor on last line and last line spans final row */
-static int render(State * st)
+static int render(State * st, uintptr_t * endp)
 {
-    printf("\33[2J\033[1;1H");
-    int cursor_c = -1, cursor_r = -1;
+    int do_output = st->redraw || st->origin != st->last_origin || st->cursor >= st->size - 1;
 
+    if (do_output) {
+        st->redraw = 0;
+        st->last_origin = st->origin;
+        printf("\33[2J\033[1;1H");
+    } else {
+        printf("\33[%d;%dH", st->ws.ws_row, 1);
+        printf("\33[2K");
+    }
+
+    int cursor_c = -1, cursor_r = -1;
     int p = st->origin;
     int r = 0;
     int c = 0;
@@ -43,7 +59,7 @@ static int render(State * st)
         cursor_r = cursor_c = 0;
 
     while (p < st->size) {
-#define OUTPUT(x) printf("%c", x); c++; if (c >= st->ws.ws_col) { c = 0; r++; if (r >= st->ws.ws_row - 1) break; }
+#define OUTPUT(x) if (do_output) printf("%c", x); c++; if (c >= st->ws.ws_col) { c = 0; r++; if (r >= st->ws.ws_row - 1) break; }
         int ch = st->buf[p];
         if (p == st->cursor) {
             cursor_c = c;
@@ -53,7 +69,7 @@ static int render(State * st)
             OUTPUT('^');
             OUTPUT(ch + 67);
             if (ch == '\n' && c != 0) {
-                printf("\n");
+                if (do_output) printf("\n");
                 c = 0;
                 r++;
                 if (r >= st->ws.ws_row - 1) break;
@@ -69,48 +85,46 @@ static int render(State * st)
     }
     int filler = 0;
     if (r < st->ws.ws_row - 1 && (st->size == 0 || (p > 0 && st->buf[p - 1] != '\n') || (st->cursor >= st->size && st->buf[st->cursor - 1] == '\n'))) {
-        printf("\n");
+        if (do_output) printf("\n");
         c = 0;
         r++;
     }
     while (r < st->ws.ws_row - 1) {
-        printf("~\n");
+        if (do_output) printf("~\n");
         r++;
         filler = 1;
     }
+
     if (st->mode == COMMAND) {
         if (st->message[0])
             printf("\033[7m%s\033[0m", st->message);
     } else if (st->mode == INPUT) {
         printf("--INSERT--");
     } else if (st->mode == PROMPT) {
-        printf(":%s", st->prompt);
-        cursor_r = r;
-        cursor_c = 1 + strlen(st->prompt);
+        printf("%s", st->prompt);
+        cursor_r = st->ws.ws_row - 1;
+        cursor_c = strlen(st->prompt);
     }
 
-    printf(" (%d/%d) %s", st->cursor, (int)st->size, cursor_r < 0 || cursor_c < 0 ? " cursor off screen" : "");
+    if (st->mode != PROMPT) {
+        printf(" (%d/%d)", st->cursor, (int)st->size);
+        if (st->count) printf(" %d", st->count);
+        if (cursor_r < 0 || cursor_c < 0) printf(" cursor off screen");
+    }
 
     if (cursor_r >= 0 || cursor_c >= 0)
         printf("\33[%d;%dH", cursor_r+1, cursor_c+1);
     fflush(stdout);
 
+    *endp = p;
+
     if (!filler && p > 0) {
         int i;
         for (i = p - 1; i >= 0 && st->buf[i] != '\n'; i--) ;
-        printf("i=%d",i);
         if (st->cursor > i)
             return 1;
     }
     return cursor_r == 0 ? -1 : 0;
-}
-
-static int getcursorchar(const State * st)
-{
-    int i;
-    for (i = st->cursor - 1; i >= 0 && st->buf[i] != '\n'; i--) ;
-    //printf("\n\n\n\n[cursor=%d, i=%d, delta=%d]\n", st.cursor, i, st.cursor - i - 1);
-    return st->cursor - i - 1;
 }
 
 static void delete(State * st)
@@ -125,13 +139,110 @@ static void delete(State * st)
         st->size--;
     }
     st->dirty = 1;
+    st->redraw = 1;
 }
 
-static void scrollup(State * st)
+static int findlinestart(const State * st, int start)
 {
-    int j;
-    for (j = st->origin - 2; j >= 0 && st->buf[j] != '\n'; j--) ;
-    st->origin = MAX(0, j + 1);
+    for (start--; start >= 0 && st->buf[start] != '\n'; start--) ;
+    return start + 1;
+}
+
+static int findlineend(const State * st, int start)
+{
+    if (!st->size)
+        return 0;
+    for (; start < st->size && st->buf[start] != '\n'; start++) ;
+    if (start == st->size)
+        start--;
+    return start;
+}
+
+static int getcursorchar(const State * st)
+{
+    return st->cursor - findlinestart(st, st->cursor);
+}
+
+static void deleteline(State * st)
+{
+    if (!st->size)
+        return;
+    int start  = findlinestart(st, st->cursor);
+    int end = findlineend(st, st->cursor);
+    int size = end - start + 1;
+    memmove(st->buf + start, st->buf + end + 1, st->size - size);
+    st->size -= size;
+    st->cursor = start;
+    if (st->size && st->cursor >= st->size)
+        st->cursor = st->size - 1;
+
+    st->dirty = 1;
+    st->redraw = 1;
+}
+
+static void scrollup(State * st, int count)
+{
+    while (count--) {
+        int j;
+        for (j = st->origin - 2; j >= 0 && st->buf[j] != '\n'; j--) ;
+        st->origin = MAX(0, j + 1);
+    }
+}
+
+static void scrolldown(State * st, int count)
+{
+    if (!st->size)
+        return;
+    while (count--) {
+        int j;
+        for (j = st->origin; j < st->size && st->buf[j] != '\n'; j++);
+        if (j >= st->size - 1)
+            for (j = st->size - 2; j >= 0 && st->buf[j] != '\n'; j--) ;
+        st->origin = j + 1;
+    }
+}
+
+static void lineup(State * st, int cursor_where)
+{
+    int cursorchar = getcursorchar(st);
+    int i;
+    for (i = st->cursor - cursorchar - 2; i >= 0 && st->buf[i] != '\n'; i--) ;
+    i++;
+    if (i < 0)
+        return;
+
+    int len;
+    for (len = 0; i + len < st->size && st->buf[i + len] != '\n'; len++);
+    cursorchar = MIN(cursorchar, len);
+
+    st->cursor = MAX(0, i + cursorchar);
+    if (cursor_where < 0)
+        scrollup(st, 1);
+}
+
+static void linedown(State * st, int cursor_where)
+{
+    if (!st->size)
+        return;
+
+    int cursorchar = getcursorchar(st);
+    int i;
+
+    if (st->buf[st->cursor] == '\n') {
+        i = st->cursor;
+    } else {
+        for (i = st->cursor + 1; i < st->size && st->buf[i] != '\n'; i++) ;
+        if (i >= st->size - 1)
+            return;
+    }
+
+    int len;
+    for (len = 0; i + 1 + len < st->size && st->buf[i + 1 + len] != '\n'; len++);
+    cursorchar = MIN(cursorchar, len);
+
+    st->cursor = MIN(st->size - 1, i + 1 + cursorchar);
+    if (cursor_where > 0)
+        scrolldown(st, 1);
 }
 
 static void flash()
@@ -169,6 +280,8 @@ static int getchar_escape()
                 } else if (modify == 2) {
                     switch(c) {
                     case 51: push_char = 'x'; break; //delete
+                    case 53: push_char = 2; break; //pgup
+                    case 54: push_char = 6; break; //pgdn
                     case 65: return 'k'; //up arrow
                     case 66: return 'j'; //down arrow
                     case 67: return 'l'; //right arrow
@@ -209,6 +322,40 @@ static int save_file(State * st, const char * path)
     snprintf(st->message, sizeof(st->message), "\"%s\" written", path);
     st->dirty = 0;
     return 0;
+}
+
+static int reallocbuf(State * st, int extra)
+{
+    char * newbuf = realloc(st->buf, st->size + extra);
+    if (!newbuf) {
+        snprintf(st->message, sizeof(st->message), "ENOMEM");
+        st->mode = COMMAND;
+        return -1;
+    }
+    st->buf = newbuf;
+    return 0;
+}
+
+static void search(State * st, int endp)
+{
+    if (!st->size || st->cursor == st->size - 1)
+        return;
+    if (reallocbuf(st, 1) < 0)
+        return;
+    st->buf[st->size] = 0;
+    char * s = strstr(st->buf + st->cursor + 1, st->prompt + 1);
+    if (!s) {
+        snprintf(st->message, sizeof(st->message), "not found");
+        return;
+    }
+    uintptr_t pos = (uintptr_t)(s - st->buf);
+    if (pos >= st->origin && pos < endp) {
+        st->cursor = pos;
+        return;
+    }
+    st->cursor = pos;
+    for (st->origin = pos - 1; st->origin >= 0 && st->buf[st->origin] != '\n'; st->origin--) ;
+    st->origin++;
 }
 
 static int vi_main(int argc, char ** argv, char ** envp)
@@ -252,100 +399,111 @@ static int vi_main(int argc, char ** argv, char ** envp)
     st.cursor = 0;
     st.dirty = 0;
     st.message[0] = 0;
+    st.count = 0;
 
     st.mode = COMMAND;
+    st.redraw = 1;
+    int repeat = 0;
     while(1) {
-        int cursor_where = render(&st);
+        uintptr_t endp;
+        int cursor_where = render(&st, &endp);
         char c = getchar_escape();
         st.message[0] = 0;
         if (st.mode == COMMAND) {
-            if (c == 'i') {
+            if ((c >= '1' && c <= '9') || (c == '0' && st.count > 0)) {
+                st.count = st.count * 10 + c - '0';
+                continue;
+            }
+            st.count = MAX(1, st.count);
+
+            if (c == 'd') {
+                repeat++;
+                if (repeat == 2) {
+                    while(st.count-- && st.size)
+                        deleteline(&st);
+                    repeat = 0;
+                }
+            } else {
+                repeat = 0;
+            }
+
+            if (c == 'd') {
+                /* avoid key unsupported message */
+            } else if (c == 'i') {
                 st.mode = INPUT;
             } else if (c == 'a') {
-                if (st.size > 0)
+                if (st.size > 0 && st.buf[st.cursor] != '\n')
                     st.cursor++;
                 st.mode = INPUT;
-            } else if (c == ':') {
+            } else if (c == ':' || c == '/') {
                 st.mode = PROMPT;
-                st.prompt[0] = 0;
+                st.prompt[0] = c;
+                st.prompt[1] = 0;
             } else if (c == '0') {
-                st.cursor -= getcursorchar(&st);
+                st.cursor = findlinestart(&st, st.cursor);
             } else if (c == '$') {
-                if (st.size > 0) {
-                    for (; st.cursor < st.size && st.buf[st.cursor] != '\n'; st.cursor++) ;
-                    if (st.cursor == st.size)
+                st.cursor = findlineend(&st, st.cursor);
+            } else if (c == 'h') {
+                while(st.count-- && getcursorchar(&st) > 0)
+                    st.cursor--;
+            } else if (c == 'l' || c == ' ') {
+                while(st.count-- && st.cursor < findlineend(&st, st.cursor))
+                    st.cursor++;
+            } else if (c == 'k') {
+                while(st.count--)
+                    lineup(&st, cursor_where);
+            } else if (c == 'j') {
+                while(st.count--)
+                    linedown(&st, cursor_where);
+            } else if (c == 2) {
+                scrollup(&st, st.ws.ws_row - 2);
+                st.cursor = st.origin;
+            } else if (c == 6) {
+                scrolldown(&st, st.ws.ws_row - 2);
+                st.cursor = st.origin;
+            } else if (c == 'b') {
+                while (st.count-- && st.size) {
+                    st.cursor--;
+                    while (st.cursor > 0 && isspace(st.buf[st.cursor])) st.cursor--;
+                    while (st.cursor > 0 && !isspace(st.buf[st.cursor])) st.cursor--;
+                    st.cursor++;
+                }
+            } else if (c == 12) {
+                st.redraw = 1;
+            } else if (c == 'w') {
+                while (st.count-- && st.size) {
+                    while (st.cursor < st.size && !isspace(st.buf[st.cursor])) st.cursor++;
+                    while (st.cursor < st.size && isspace(st.buf[st.cursor])) st.cursor++;
+                    if (st.cursor >= st.size)
                         st.cursor = st.size - 1;
                 }
-            } else if (c == 'h') {
-                int cursorchar = getcursorchar(&st);
-                if (cursorchar > 0)
-                    st.cursor--;
-                else
-                    flash();
-            } else if (c == 'l') {
-                if (st.size && st.cursor < st.size - 1 && st.buf[st.cursor] != '\n')
-                    st.cursor++;
-                else
-                    flash();
-            } else if (c == 'k') {
-                int cursorchar = getcursorchar(&st);
-                int i;
-                for (i = st.cursor - cursorchar - 2; i >= 0 && st.buf[i] != '\n'; i--) ;
-                i++;
-                if (i < 0) {
-                    flash();
-                    continue;
-                }
-
-                int len;
-                for (len = 0; i + len < st.size && st.buf[i + len] != '\n'; len++);
-                cursorchar = MIN(cursorchar, len);
-
-                st.cursor = MAX(0, i + cursorchar);
-
-                if (cursor_where < 0)
-                    scrollup(&st);
-
-            } else if (c == 'j') {
-                int cursorchar = getcursorchar(&st);
-                int i;
-                if (!st.size) {
-                    continue;
-                } else if (st.buf[st.cursor] == '\n') {
-                    i = st.cursor;
-                } else {
-                    for (i = st.cursor + 1; i < st.size && st.buf[i] != '\n'; i++) ;
-                    if (i >= st.size - 1) {
-                        flash();
-                        continue;
-                    }
-                }
-
-                int len;
-                for (len = 0; i + 1 + len < st.size && st.buf[i + 1 + len] != '\n'; len++);
-                cursorchar = MIN(cursorchar, len);
-
-                st.cursor = MIN(st.size - 1, i + 1 + cursorchar);
-
-                if (cursor_where > 0) {
-                    int j;
-                    for (j = st.origin; j < st.size && st.buf[j] != '\n'; j++);
-                    st.origin = j + 1;
+            } else if (c == 'G') {
+                if (st.size) {
+                    st.cursor = st.size - 1;
+                    st.origin = findlinestart(&st, st.cursor);
                 }
             } else if (c == 27) {
                 flash();
             } else if (c == '\b' || c == 127) { //backspace
-                if (st.cursor > 0) {
+                if (st.count-- && st.cursor > 0) {
                     st.cursor--;
                     delete(&st);
                 }
-            } else if (c == 'd' || c == 'x') {
-                delete(&st);
-                if (st.size && st.cursor == st.size)
-                    st.cursor--;
+            } else if (c == 'x') {
+                while (st.count-- && st.size) {
+                    delete(&st);
+                    if (st.size && st.cursor == st.size)
+                        st.cursor--;
+                }
+            } else if (c == 'n') {
+                if (st.prompt[0] == '/')
+                    while (st.count--)
+                        search(&st, endp);
             } else {
-                snprintf(st.message, sizeof(st.message), "%c key unsupported", c);
+                snprintf(st.message, sizeof(st.message), "%d key unsupported", c);
             }
+
+            st.count = 0;
         } else if (st.mode == INPUT) {
             if (c == 27) {
                 if (st.cursor > 0)
@@ -357,17 +515,16 @@ static int vi_main(int argc, char ** argv, char ** envp)
                     delete(&st);
                 }
             } else {
-                st.buf = realloc(st.buf, st.size + 1);
-                if (!st.buf) {
-                    snprintf(st.message, sizeof(st.message), "ENOMEM");
-                    st.mode = COMMAND;
+                if (reallocbuf(&st, 1) < 0)
                     continue;
-                }
                 if (st.cursor < st.size)
                     memmove(st.buf + st.cursor + 1, st.buf + st.cursor, st.size - st.cursor);
                 st.size += 1;
                 st.buf[st.cursor++] = c;
                 st.dirty = 1;
+                st.redraw = 1;
+                if (c == '\n' && cursor_where > 0)
+                    scrolldown(&st, 1);
             }
         } else if (st.mode == PROMPT) {
             if (c == 27) {
@@ -376,28 +533,40 @@ static int vi_main(int argc, char ** argv, char ** envp)
                 if (strlen(st.prompt) > 0)
                     st.prompt[strlen(st.prompt) - 1] = 0;
             } else if (c == '\n') {
-                for (char * s = st.prompt; *s; s++) {
-                    if (*s == 'q') {
-                        if (st.dirty && s[1] != '!') {
-                            snprintf(st.message, sizeof(st.message), "dirty buffer");
-                            st.mode = COMMAND;
-                            continue;
-                        }
-                        goto quit;
-                    } else if (*s == 'w') {
-                        const char * path = (s[1] == ' ' && s[2]) ? s + 2 : NULL;
-                        if (!st.orig_path[0] && !path) {
-                            snprintf(st.message, sizeof(st.message), "no file name");
-                            st.mode = COMMAND;
-                            continue;
-                        }
-                        if (save_file(&st, path ? path : st.orig_path) < 0) {
-                            st.mode = COMMAND;
-                            continue;
-                        }
-                        if (path)
+                if (st.prompt[0] == ':') {
+                    for (char * s = st.prompt + 1; *s; s++) {
+                        if (*s == 'q') {
+                            if (st.dirty && s[1] != '!') {
+                                snprintf(st.message, sizeof(st.message), "dirty buffer");
+                                break;
+                            }
+                            goto quit;
+                        } else if (*s == 'w') {
+                            const char * path = (s[1] == ' ' && s[2]) ? s + 2 : NULL;
+                            if (!st.orig_path[0] && !path) {
+                                snprintf(st.message, sizeof(st.message), "no file name");
+                                break;
+                            }
+                            if (save_file(&st, path ? path : st.orig_path) < 0)
+                                break;
+                            if (path)
+                                break;
+                        } else if (*s == 'x') {
+                            if (st.orig_path[0]) {
+                                if (save_file(&st, st.orig_path) < 0)
+                                    break;
+                            }
+                            goto quit;
+                        } else if (*s == '0') {
+                            st.origin = 0;
+                            st.cursor = 0;
+                        } else {
+                            snprintf(st.message, sizeof(st.message), "%c command unsupported", *s);
                             break;
+                        }
                     }
+                } else if (st.prompt[0] == '/') {
+                    search(&st, endp);
                 }
                 st.mode = COMMAND;
             } else {
