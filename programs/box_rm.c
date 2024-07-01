534dfcab482fa46309d70cef349d269a06a72aba
Peter Ross <pross@xvid.org>
2024-07-02 08:11:19 +1000

    programs: rm: display errno

--- programs/box_rm.c
+++ programs/box_rm.c
@@ -1,11 +1,13 @@
+#include <errno.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 static int rm_main(int argc, char ** argv, char ** envp)
 {
     for (int i = 1; i < argc; i++) {
         if (unlink(argv[i]) < 0) {
-            printf("cannot remove '%s'\n", argv[i]);
+            printf("cannot remove '%s': %s\n", argv[i], strerror(errno));
             return -1;
         }
     }
