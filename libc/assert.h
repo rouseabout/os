1fd632cc07481e6816db549f0e379933b46d421f
Peter Ross <pross@xvid.org>
2024-07-10 08:41:54 +1000

    Revert "libc: assert"
    This reverts commit aa17c14e2b3d9b8cdbf85a0147e0e4ccfe54ae6a.

--- libc/assert.h
+++ libc/assert.h
@@ -1,7 +1,7 @@
 #ifndef ASSERT_H
 #define ASSERT_H
 
-#define assert(expr) do { if (expr) {} } while(0)
+#define assert(ignore) ((void) 0)
 #define static_assert _Static_assert
 
 #endif /* ASSERT_H */
