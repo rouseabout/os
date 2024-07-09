4df2224a0b6588f933be70cf0bbad42e2191cb3a
Peter Ross <pross@xvid.org>
2024-07-09 11:23:32 +1000

    ports: sc: ignore warnings

--- ports/sc/port.sh
+++ ports/sc/port.sh
@@ -6,5 +6,5 @@ set -e
 VERSION=7.16
 prep http://www.ibiblio.org/pub/Linux/apps/financial/spreadsheet/sc-${VERSION}.tar.gz sc-${VERSION}.tar.gz sc-${VERSION}
 
-make CC=${ARCH}-pc-elf-gcc CFLAGS="-DSYSV3 -I${SYSROOT}/usr/local/include/ncurses" LIB="-lm -lncurses -ltinfo" -j$(nproc) 
+make CC=${ARCH}-pc-elf-gcc CFLAGS="-Wno-implicit-function-declaration -Wno-return-mismatch -Wno-implicit-int -DSYSV3 -I${SYSROOT}/usr/local/include/ncurses" LIB="-lm -lncurses -ltinfo" -j$(nproc)
 make prefix=$SYSROOT/usr/local install
