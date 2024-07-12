6da1a0aaeeb1baf9a6e8c6f83f59375572ccd086
Peter Ross <pross@xvid.org>
2024-07-12 20:19:21 +1000

    programs: cal: yearly calendar

--- programs/box_cal.c
+++ programs/box_cal.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <langinfo.h>
 #include <time.h>
 
@@ -10,19 +11,11 @@ static int is_common_year(int y)
 static int mdays(int y, int m)
 {
     static const int dim[12] = {31,29,31,30,31,30,31,31,30,31,30,31};
-    return (m == 1 && is_common_year(y)) ? 28 : dim[m - 1];
+    return (m == 1 && is_common_year(y)) ? 28 : dim[m];
 }
 
-static int cal_main(int argc, char ** argv, char ** envp)
+static void print_cal(struct tm tm, int today)
 {
-    time_t t;
-    struct tm tm;
-    t = time(NULL);
-    localtime_r(&t, &tm);
-    int today = tm.tm_mday;
-    tm.tm_mday = 1;
-    t = mktime(&tm);
-    localtime_r(&t, &tm);
     printf("%s %d\n", nl_langinfo(MON_1 + tm.tm_mon), 1900 + tm.tm_year);
     printf("Su Mo Tu We Th Fr Sa\n");
     int wday = tm.tm_wday;
@@ -35,5 +28,29 @@ static int cal_main(int argc, char ** argv, char ** envp)
     }
     if (wday < 7)
         printf("\n");
-    return 0;
+}
+
+static int cal_main(int argc, char ** argv, char ** envp)
+{
+    if (argc == 2) {
+        int year = atoi(argv[1]);
+        for (int i = 0; i < 12; i++) {
+            struct tm tm = {.tm_year = year - 1900, .tm_mon = i, .tm_mday = 1};
+            time_t t = mktime(&tm);
+            localtime_r(&t, &tm);
+            print_cal(tm, 0);
+            printf("\n");
+        }
+    } else {
+        time_t t;
+        struct tm tm;
+        t = time(NULL);
+        localtime_r(&t, &tm);
+        int today = tm.tm_mday;
+        tm.tm_mday = 1;
+        t = mktime(&tm);
+        localtime_r(&t, &tm);
+        print_cal(tm, today);
+    }
+    return EXIT_SUCCESS;
 }
