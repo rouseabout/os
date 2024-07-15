ab7e9c92a274ab0e54990580a45fec470a993b13
Peter Ross <pross@xvid.org>
2024-07-15 17:27:32 +1000

    programs: xargs: NMAX

--- programs/box_xargs.c
+++ programs/box_xargs.c
@@ -5,39 +5,47 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-static void run2(int argc1, char ** argv1, int argc2, char ** argv2)
+#define NMAX 5
+
+static void run2(int argc1, char ** argv1, int argc2, char argv2[NMAX][PATH_MAX])
 {
-    char ** argv = malloc((argc1 + argc2 + 1) * sizeof(char *));
-    if (!argv) {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
-    for (int i = 0; i < argc1; i++)
-        argv[i] = argv1[i];
-    for (int i = 0; i < argc2; i++)
-        argv[argc1 + i] = argv2[i];
-    argv[argc1 + argc2] = NULL;
-    execvp(argv[0], argv);
+    pid_t pid = fork();
+    if (pid < 0)
+        perror("fork");
+    else if (!pid) {
+        char ** argv = malloc((argc1 + argc2 + 1) * sizeof(char *));
+        if (!argv) {
+            perror("malloc");
+            exit(EXIT_FAILURE);
+        }
+        for (int i = 0; i < argc1; i++)
+            argv[i] = argv1[i];
+        for (int i = 0; i < argc2; i++)
+            argv[argc1 + i] = argv2[i];
+        argv[argc1 + argc2] = NULL;
+        execvp(argv[0], argv);
+    } else
+        wait(NULL);
 }
 
 static int xargs_main(int argc, char ** argv, char ** envp)
 {
     int ret = 0;
-    char path[PATH_MAX];
+    char path[NMAX][PATH_MAX];
+    int n = 0;
     do {
         int i;
-        for (i = 0; i < sizeof(path) - 1 && (ret = read(STDIN_FILENO, &path[i], 1)) == 1 && !isspace(path[i]); i++) ;
+        for (i = 0; i < sizeof(path[n]) - 1 && (ret = read(STDIN_FILENO, &path[n][i], 1)) == 1 && !isspace(path[n][i]); i++) ;
         if (!i)
             continue;
-        path[i] = 0;
-        pid_t pid = fork();
-        if (pid < 0)
-            perror("fork");
-        else if (!pid) {
-            char * path2 = path;
-            run2(argc - 1, argv + 1, 1, &path2);
-        } else
-            wait(NULL);    
+        path[n][i] = 0;
+        n++;
+        if (n >= NMAX) {
+            run2(argc - 1, argv + 1, n, path);
+            n = 0;
+        }
     } while (ret == 1);
+    if (n)
+        run2(argc - 1, argv + 1, n, path);
     return EXIT_SUCCESS;
 }
