927128c6041dc7da69ab4a7d9e208083150f3564
Peter Ross <pross@xvid.org>
2024-07-17 18:30:45 +1000

    programs: cat: use stdlib

--- programs/box_cat.c
+++ programs/box_cat.c
@@ -1,5 +1,6 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 static int cat_main(int argc, char ** argv, char ** envp)
@@ -9,17 +10,14 @@ static int cat_main(int argc, char ** argv, char ** envp)
         fd = open(argv[1], O_RDONLY);
         if (fd < 0) {
             perror(argv[1]);
-            return -1;
+            return EXIT_FAILURE;
         }
-   } else
+    } else
         fd = STDIN_FILENO;
-
     char block[1024];
     int size;
     while ((size = read(fd, block, sizeof(block))) > 0)
         write(STDOUT_FILENO, block, size);
-
     close(fd);
-
-    return 0;
+    return EXIT_SUCCESS;
 }
