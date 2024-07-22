e955eb9015c40a2f42ea96620d251cd6732f2121
Peter Ross <pross@xvid.org>
2024-07-22 18:00:39 +1000

    programs: rm, rmdir: use perror

--- programs/box_rm.c
+++ programs/box_rm.c
@@ -1,15 +1,15 @@
-#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 static int rm_main(int argc, char ** argv, char ** envp)
 {
     for (int i = 1; i < argc; i++) {
-        if (unlink(argv[i]) < 0) {
-            printf("cannot remove '%s': %s\n", argv[i], strerror(errno));
-            return -1;
+        if (unlink(argv[i]) == -1) {
+            perror(argv[i]);
+            return EXIT_FAILURE;
         }
     }
-    return 0;
+    return EXIT_SUCCESS;
 }
--- programs/box_rmdir.c
+++ programs/box_rmdir.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 static int rmdir_main(int argc, char ** argv, char ** envp)
 {
     for (int i = 1; i < argc; i++) {
-        if (rmdir(argv[i]) < 0) {
-            printf("cannot remove '%s'\n", argv[i]);
-            return -1;
+        if (rmdir(argv[i]) == -1) {
+            perror(argv[i]);
+            return EXIT_FAILURE;
         }
     }
-    return 0;
+    return EXIT_SUCCESS;
 }
