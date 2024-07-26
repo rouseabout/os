aed7bfdc146d8bfb9c11827a747a46c6ddd5b7c6
Peter Ross <pross@xvid.org>
2024-07-26 20:07:17 +1000

    ports: xz

--- ports/xz/port.sh
+++ ports/xz/port.sh
@@ -3,7 +3,7 @@ set -e
 
 . ../common.sh
 
-VERSION=5.6.1
+VERSION=5.6.2
 prep https://github.com/tukaani-project/xz/releases/download/v${VERSION}/xz-${VERSION}.tar.xz xz-${VERSION}.tar.xz xz-${VERSION}
 
 ./configure --host=${ARCH}-pc-elf --enable-threads=no
