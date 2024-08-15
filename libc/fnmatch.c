23d3620846fb1422749cbd16c1ab5cc1dc76d712
Peter Ross <pross@xvid.org>
2024-08-15 11:29:30 +1000

    libc: fnmatch

--- libc/fnmatch.c
+++ libc/fnmatch.c
@@ -1,9 +1,10 @@
 #include <fnmatch.h>
 #include <stdio.h>
+#include <string.h>
 #include <syslog.h>
 
 int fnmatch(const char *pattern, const char *string, int flags)
 {
     syslog(LOG_DEBUG, "libc: fnmatch");
-    return 1; //FIXME:
+    return strstr(string, pattern) ? 0 : FNM_NOMATCH; //FIXME:
 }
