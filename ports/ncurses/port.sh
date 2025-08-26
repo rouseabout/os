af3746ac227267060144567c4edacf4e7c7a9a9d
Peter Ross <pross@xvid.org>
2025-08-26 18:38:33 +1000

    ports: ncurses

--- ports/ncurses/port.sh
+++ ports/ncurses/port.sh
@@ -3,9 +3,9 @@ set -e
 
 . ../common.sh
 
-VERSION=6.3
+VERSION=6.4
 prep https://invisible-mirror.net/archives/ncurses/ncurses-${VERSION}.tar.gz ncurses-${VERSION}.tar.gz ncurses-${VERSION}
 
-./configure --host=${ARCH}-pc-elf --without-ada --without-cxx --with-termlib #--disable-database --with-fallbacks=vt100
+./configure --host=${ARCH}-pc-elf --without-ada --without-cxx --with-termlib --disable-database --with-fallbacks=vt100
 make -j$(nproc)
 make DESTDIR=$SYSROOT install
