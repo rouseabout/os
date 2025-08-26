9438e8c09a6ef76c4e3c12d7fdbf91fa6f7cf63f
Peter Ross <pross@xvid.org>
2025-08-26 18:44:20 +1000

    kernel: linux compatible waitpid

--- libc/sys_wait.c
+++ libc/sys_wait.c
@@ -6,4 +6,12 @@ pid_t wait(int * stat_loc)
     return waitpid((pid_t)-1, stat_loc, 0);
 }
 
+#if defined(ARCH_x86_64)
+static MK_SYSCALL4(pid_t, wait4, OS_WAITPID, pid_t, int *, int, void *)
+pid_t waitpid(pid_t pid, int * stat_loc, int options)
+{
+    return wait4(pid, stat_loc, options, NULL);
+}
+#else
 MK_SYSCALL3(pid_t, waitpid, OS_WAITPID, pid_t, int *, int)
+#endif
