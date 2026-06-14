bc6b1bc0026b6cf97eada284a72390d9359d6658
Peter Ross <pross@xvid.org>
2026-06-14 16:57:13 +1000

    programs: sleep: fractional sleep

--- programs/box_sleep.c
+++ programs/box_sleep.c
@@ -1,10 +1,13 @@
-#include <unistd.h>
+#include <math.h>
 #include <stdlib.h>
+#include <time.h>
 
 static int sleep_main(int argc, char ** argv, char ** envp)
 {
     if (argc != 2)
         return -1;
-    sleep(atoi(argv[1]));
+    double ti, tf;
+    tf = modf(atof(argv[1]), &ti);
+    nanosleep(&(struct timespec){.tv_sec=ti, .tv_nsec=tf*1000000000}, NULL);
     return 0;
 }
